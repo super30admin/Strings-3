1b4d518e28ada53536c59f0e966d433a4a46e9e7
JacobStephen9999 <52669930+JacobStephen9999@users.noreply.github.com>
2020-06-14 20:15:02 -0400

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,22 @@
+#Time COmplexity : O( N) whereN is number of elements 
+#Space Complexity : o(N)
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        lastsign ="+"
+        num = 0
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num *10 + int(s[i])
+            if ((not s[i].isdigit()) and s[i]!=" ") or i == len(s) -1:
+                if lastsign == "+":
+                    stack.append(num)
+                if lastsign == "-":
+                    stack.append(-num)
+                if lastsign == "*":
+                    stack.append(stack.pop() *num)
+                if lastsign == "/":
+                    stack.append(int(stack.pop()/num))
+                lastsign = s[i]
+                num = 0
+        return sum(stack)
