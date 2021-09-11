9d3e8629fc011a719db4193ab1d64adb4ab3bae3
Srinivasa Rao Yedla <ysrinu15@gmail.com>
2021-09-11 14:27:07 -0700

    String-3

---
+++ BasicCalculatorii-SC(1).java
@@ -0,0 +1,45 @@
+
+
+// Time Complexity : o(n)
+// Space Complexity : 0(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+public class BasicCalculatorii {
+    public int calculate(String s) {
+    if(s == null || s.length() == 0) {
+    return 0;
+    }
+    int num = 0;
+    int result = 0;
+    char lastSign= '+';
+    Stack<Integer> st = new Stack<>();
+    for(int i=0; i< s.length(); i++) {
+    char c = s.charAt(i);
+    if(Character.isDigit(c)){
+    num = num * 10 + c- '0';
+    }
+    if((!Character.isDigit(c) && c!= ' ') || (i == s.length() - 1)){
+    if (lastSign == '+') 
+    {
+    st.push(num);
+    }
+    else if( lastSign == '-'){
+    st.push(-num);
+    }
+    else if(lastSign == '*') {
+    st.push(st.pop() * num );
+    }
+    else {
+    st.push(st.pop() / num );
+    }
+    lastSign = c ;
+    num = 0;
+    }
+    }
+    while( !st.isEmpty()){
+    result += st.pop();
+    }
+    return result ;
+    }
+    }
