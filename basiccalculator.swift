c926a0183d9372cff2fd411bfb4f5cccdd7f0a61
panth1921 <panthdev96@gmail.com>
2021-11-17 23:59:58 -0500

    Strings-3

---
+++ basiccalculator.swift
@@ -0,0 +1,45 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+        func calculate(_ s: String) -> Int {
+        var stack = [Int]()
+        var sArray = Array(s)
+        var i = 0
+        var sign = "+"
+        var num = 0
+        var output = 0
+        while i < sArray.count {
+            
+            var ch = sArray[i]
+            
+            if ch.isNumber {
+                num = num * 10 + (ch.wholeNumberValue ?? 0)
+            }
+            if (ch != " " && !ch.isNumber) || i == sArray.count - 1  {
+                
+                if sign == "+" {
+                    stack.append(num)
+                } else if sign == "-" {
+                    stack.append(-num)
+                } else if sign == "*" {
+                    var ans = stack.removeLast() * num
+                    stack.append(ans)
+                } else if sign == "/" {
+                    var ans = stack.removeLast() / num
+                    stack.append(ans)
+                }
+                sign = String(ch)
+                num = 0
+            }
+            i += 1
+        }
+        
+        for value in stack {
+            output += value
+        } 
+        return output
+    }
