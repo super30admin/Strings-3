a8bfd8bc1477525e6905ae501a8fdad8abcc86c0
surajbodapati <lbodapat@buffalo.edu>
2022-05-24 21:28:01 -0400

    Strings3 .1

--- IntegerToWord.java
+++ IntegerToWord.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class IntegerToWord {
+// Time Complexity : O(1) - where N is number of digits which is constant.
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No.
+
+// Your code here along with comments explaining your approach
+/*
+Divide the integer into 3 digits at a time. Append the subproblem result.
+* */
+public class IntegerToWord {
+    String[] thousands={"","Thousand", "Million", "Billion"};
+    String[] below20= {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen",
+            "Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    String[] tens={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    public String numberToWords(int num) {
+        if(num==0) return "Zero";
+
+        String result= "";
+        int i=0;
+        while(num>0){
+            if(num%1000 != 0){
+                result= helper(num%1000) + thousands[i]+" "+ result;
+            }
+            i++;
+            num/=1000;
+        }
+
+        return result.trim();
+    }
+
+    private String helper(int num){
+        if(num==0) return "";
+        else if(num<20) return below20[num]+" ";
+        else if(num<100) return tens[num/10]+" "+helper(num%10);
+        else{
+            return below20[num/100]+" Hundred "+helper(num%100);
+        }
+    }
 }
