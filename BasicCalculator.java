4ff38660234eb16d7ab2f6b7ec11a086f0729859
Shivani31996 <shivani.chavan96@gmail.com>
2022-11-19 12:22:38 -0500

    Adding approach 2

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -49,3 +49,53 @@ public class BasicCalculator {
         return result;
     }
 }
+
+/*
+ * Approach 2: Evaluate without using a Stack; This method is specfic to this problem.
+ * 1 - Maintain a number, calculated value and a tail. Incoming number is num.
+ * 2 - If * comes, we need to subtract the tail from the calculated value and multiply that tail with incoming num
+ */
+class Solution {
+    public int calculate(String s) {
+        int num = 0;
+        int calc = 0;
+        int tail = 0;
+        
+        int lastSign = '+';
+        
+        for(int i = 0; i < s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(Character.isDigit(c))
+            {
+                num = num*10 + c - '0';
+            }
+            if((!Character.isDigit(c) && c != ' ') || i == s.length() -1 )
+            {
+                if(lastSign == '+')
+                {
+                    calc = calc + num;
+                    tail = num;
+                }
+                else if(lastSign == '-')
+                {
+                    calc = calc - num;
+                    tail = -num;
+                }
+                else if(lastSign == '*')
+                {
+                    calc = calc - tail + tail * num;
+                    tail = num * tail;
+                }
+                else if(lastSign == '/')
+                {
+                    calc = calc - tail + tail/num;
+                    tail = tail/num;
+                }
+                num = 0;
+                lastSign = c;
+            }
+        }
+        return calc;
+    }
+}
