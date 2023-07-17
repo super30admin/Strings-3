0d9af1eb9eacc06f6659e01d68e944ebcd89082e
malfusion2 <judenaveenraj@icloud.com>
2023-07-16 17:08:18 -0700

    Added complexities

--- Problem2.py
+++ Problem2.py
@@ -1,5 +1,5 @@
-# Time Complexity : O()
-# Space Complexity : O()
+# Time Complexity : O(s) where s is the length of the string
+# Space Complexity : O(1)
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
